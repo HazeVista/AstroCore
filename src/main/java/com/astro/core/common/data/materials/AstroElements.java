c4233427b9f9014b82a6609cafec3b7d51b44786
HazeVista <zetateamhazevista@gmail.com>
2025-12-10 15:09:12 +0000

    0.0.1 12-9-25 Haze extendra setIgnored

--- src/main/java/com/astro/core/common/data/materials/AstroElements.java
+++ src/main/java/com/astro/core/common/data/materials/AstroElements.java
@@ -5,7 +5,12 @@ import com.gregtechceu.gtceu.api.registry.GTRegistries;
 
 public class AstroElements {
 
-    public static final Element APNS = createAndRegister(-1, -1, -1, null, "mystery", "✨", false);
+    public static final Element DE = createAndRegister(-1, -1, -1, null, "desh", "De", false);
+    public static final Element OT = createAndRegister(-1, -1, -1, null, "ostrum", "Ot", false);
+    public static final Element CT = createAndRegister(-1, -1, -1, null, "calorite", "Ct", false);
+    public static final Element JP = createAndRegister(-1, -1, -1, null, "juperium", "Jp", false);
+    public static final Element SY = createAndRegister(-1, -1, -1, null, "saturlyte", "Sy", false);
+    public static final Element E = createAndRegister(-1, -1, -1, null, "electrolyte", "⚡", false);
 
     public static Element createAndRegister(long protons, long neutrons, long halfLifeSeconds, String decayTo,
                                             String name, String symbol, boolean isIsotope) {
