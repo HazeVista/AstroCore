59aed7f688fe511c18e734fe10efe9f3f85a16dd
Connor Smith <phoenixvine32908@gmail.com>
2026-01-17 09:04:56 -0500

    fix mixins.

--- src/main/java/com/astro/core/mixin/AstroCoreMixinPlugin.java
+++ src/main/java/com/astro/core/mixin/AstroCoreMixinPlugin.java
@@ -3,6 +3,7 @@ package com.astro.core.mixin;
 import org.objectweb.asm.tree.ClassNode;
 import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
 import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
+import net.minecraftforge.fml.loading.LoadingModList;
 
 import java.util.List;
 import java.util.Set;
@@ -14,13 +15,22 @@ public class AstroCoreMixinPlugin implements IMixinConfigPlugin {
 
     @Override
     public String getRefMapperConfig() {
-        return "";
+        // Must match the "refmap" entry in your astrocore.mixins.json
+        return "mixins.astrocore.refmap.json";
     }
 
     @Override
     public boolean shouldApplyMixin(String targetClassName, String mixinClassName) {
-        if (mixinClassName.equals("com.astro.core.mixin.MixinCutterHook")) {
-            return net.minecraftforge.fml.loading.LoadingModList.get().getModFileById("extendedae") != null;
+        // List of mixins that strictly require Extended AE to function
+        List<String> conditionalMixins = List.of(
+                "com.astro.core.mixin.MixinContainerRenamer",
+                "com.astro.core.mixin.MixinCutterHook",
+                "com.astro.core.mixin.MixinMEPatternBufferCustomName"
+        );
+
+        if (conditionalMixins.contains(mixinClassName)) {
+            // Returns true only if Extended AE is actually loaded
+            return LoadingModList.get().getModFileById("extendedae") != null;
         }
         return true;
     }
@@ -29,9 +39,7 @@ public class AstroCoreMixinPlugin implements IMixinConfigPlugin {
     public void acceptTargets(Set<String> myTargets, Set<String> otherTargets) {}
 
     @Override
-    public List<String> getMixins() {
-        return List.of();
-    }
+    public List<String> getMixins() { return List.of(); }
 
     @Override
     public void preApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {}
