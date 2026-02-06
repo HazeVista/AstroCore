3c6d79e3b354b9cf7129f81f1dbc7fdc3904ecaf
digestlotion <keithersuars@gmail.com>
2026-02-06 13:33:30 +0800

    conduit router logic

--- src/main/java/com/astro/core/common/data/AstroItems.java
+++ src/main/java/com/astro/core/common/data/AstroItems.java
@@ -1,5 +1,8 @@
 package com.astro.core.common.data;
 
+import com.astro.core.common.data.item.conduit_router.ConduitRouterItem;
+import com.tterrag.registrate.util.entry.ItemEntry;
+
 import static com.astro.core.AstroCore.ASTRO_CREATIVE_TAB;
 import static com.astro.core.common.registry.AstroRegistry.REGISTRATE;
 
@@ -9,6 +12,12 @@ public class AstroItems {
     static {
         REGISTRATE.creativeModeTab(() -> ASTRO_CREATIVE_TAB);
     }
+
+//    public static ItemEntry<ConduitRouterItem> CONDUIT_ROUTER = REGISTRATE
+//            .item("conduit_router", ConduitRouterItem::new)
+//            .properties(p -> p.stacksTo(1))
+//            .lang("Conduit Router")
+//            .register();
 
     public static void init() {}
 }
